8e5b064f86995e1d5d638f85981234c26af6750d
Sean Garfola <sagarfola@gmail.com>
2020-07-23 21:26:53 -0400

    Commit 23 Jul

---
+++ Dnd Character Generator/Armor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnd_Character_Generator
+{
+    class Armor
+    {
+        private string name;
+        private int armorClass;
+        private int cost;
+        private int weight;
+
+        public Armor(string name)
+        { 
+            if (name == "Leather") {
+                armorClass = 7;
+                cost = 20;
+                weight = 200;
+            } else if (name == "Chain mail") {
+                armorClass = 5;
+                cost = 40;
+                weight = 400;
+            } else if (name == "Plate mail") {
+                armorClass = 3;
+                cost = 60;
+                weight = 500;
+            } else if (name == "Shield") {
+                armorClass = -1;
+                cost = 10;
+                weight = 100;
+            } else {
+                name = "Unarmored";
+                armorClass = 9;
+                cost = 0;
+                weight = 0;
+            }
+        }
+    }
+}
--- Dnd Character Generator/Program.cs
+++ Dnd Character Generator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,9 +30,17 @@ namespace Dnd_Character_Generator
              */
 
             // initialize variables needed for the program
-            int[] abilityScore = new int[6];    // Ability scores in index 0 to 5 that correspond to STR, INT, WIS, DEX, CON, CHA respectively
-            int[] abilityMod = new int[6];      // Ability score modifiers based on the +3 to -3 scale corresponding to the appropriate stat
-            string charClass;                   // Stores a string referring to a character classes and demihumans    
+            int[] abilityScore = new int[6];                            // Ability scores in index 0 to 5 that correspond to STR, INT, WIS, DEX, CON, CHA respectively
+            int[] abilityMod = new int[6];                              // Ability score modifiers based on the +3 to -3 scale corresponding to the appropriate stat
+            string charClass;                                           // Stores a string referring to a character classes and demihumans
+            int armorClass, thaco;                                      // AC and Thac0 values
+            int death, wands, paralysis, breath, spells;                // Saving throw values
+            int opendoors, language, loyalty, max_retain, min_retain;   // Values to hold open door check value, # of languages known, loyalty, and min/max retainers 
+            int melatk, rngatk;                                         // Attack modifiers
+            int xp, xpNext, xpMod;                                      // Current Xp, XP to next level, Xp modifier based on prime requistite
+            int hp;                                                     // Maximum HP
+            int listenDoors, findTrap, findSecretDoor;                  // Standard listen at doors, find room traps, and finding secret doors
+
 
             // Define the random seed to use for all die rolls
             Random rnd = new Random();
@@ -49,16 +58,65 @@ namespace Dnd_Character_Generator
             }
 
             // ** CHOOSE CLASS **
+            charClass = GetClass(abilityScore[1], abilityScore[3], abilityScore[4]);
 
+            // ** USE MODIFIERS TO GENERATE VARIOUS SCORES
+            opendoors = 2 + abilityMod[0];      // Open Doors Check
+
+            melatk = abilityMod[0];             // Melee attack bonus
+
+            language = abilityMod[1];           // Additional Languages known
+
+            if (charClass == "Halfling") {      // Range attack bonus (Halflings get an additional +1)
+                rngatk = abilityMod[3] + 1;
+            } else {
+                rngatk = abilityMod[3];
+            }
+
+            if (charClass == "Elf") {           // Listen at doors, find traps, and find secret door checks
+                listenDoors = 2;
+                findSecretDoor = 2;
+                findTrap = 1;
+            } else if (charClass == "Dwarf") {
+                listenDoors = 2;
+                findTrap = 2;
+                findSecretDoor = 1;
+            } else if (charClass == "Halfling") {
+                listenDoors = 2;
+                findTrap = 1;
+                findSecretDoor = 1;
+            } else {
+                listenDoors = 1; 
+                findTrap = 1; 
+                findSecretDoor = 1;
+            }
+
+            // ** GENERATE HIT POINTS
+            hp = GetHP(charClass, abilityMod[4]);
+
+            Console.WriteLine("Class: " + charClass);
+            Console.WriteLine("Max HP: " + hp);
             Console.WriteLine("Str: " + abilityScore[0] + " | " + abilityMod[0]);
             Console.WriteLine("Int: " + abilityScore[1] + " | " + abilityMod[1]);
             Console.WriteLine("Wis: " + abilityScore[2] + " | " + abilityMod[2]);
             Console.WriteLine("Dex: " + abilityScore[3] + " | " + abilityMod[3]);
             Console.WriteLine("Con: " + abilityScore[4] + " | " + abilityMod[4]);
             Console.WriteLine("Cha: " + abilityScore[5] + " | " + abilityMod[5]);
+            Console.WriteLine("Melee Attack Mod: " + melatk);
+            Console.WriteLine("Ranged Attack Mod: " + rngatk);
+            Console.WriteLine("Additional Languages Known: " + language);
+            Console.WriteLine("Open Doors: " + opendoors + "-in-6");
+            Console.WriteLine("Listen at Doors: " + listenDoors + "-in-6");
+            Console.WriteLine("Find Room Traps: " + findTrap + "-in-6");
+            Console.WriteLine("Find Secret Doors: " + findSecretDoor + "-in-6");
+
+
+            ReadCsv("C:/Users/sagar/source/repos/Dnd Character Generator/Dnd Character Generator/SavingThrows_THAC0.csv");
+
 
             Console.ReadLine();
 
+            // *** FUNCTIONS UTILIZING RANDOM VALUES
             int RollDice(int dieNum, int dieSize)
                 // This function rolls a [dieNum] number of dice of
                 //   [dieSize] size and adds the results which is passed
@@ -76,19 +134,93 @@ namespace Dnd_Character_Generator
                 return result;
             }
 
-            void GetClass(int[] stat)
+            string GetClass(int intelligence, int dexterity, int constitution)
             {
                 // This method randomly chooses a character class accounting for class restrictions
                 //  currently does not optimize class choice
 
-                int classChoice;    // stores the random choice value to select the class based on a switch case
+                int classChoice = 0;    // stores the random choice value to select the class based on a switch case
+                string result;
 
-                if (abilityScore[1] > 9 && abilityScore[3] > 9 && abilityScore[4] > 9)
+                if (intelligence < 9 && constitution < 9)  // Only Human Classes
                 {
-                    classChoice = rnd.Next(1, 8);   // .Next function has upper bound as exclusive
-
-                    
+                    classChoice = rnd.Next(1, 5);
+                } else if (intelligence > 8 && constitution < 9)  // Elf, No Dwarf, No Halfling
+                {
+                    classChoice = rnd.Next(1, 6);
+                } else if (intelligence < 9 && dexterity > 8 && constitution > 8)    // No Elf
+                {
+                    classChoice = rnd.Next(1, 8);
+                    if (classChoice == 5)
+                    {
+                        classChoice = 0;
+                    }
+                } else if (intelligence < 9 && dexterity < 9 && constitution > 8)    // No Elf, No Halfling
+                {
+                    classChoice = rnd.Next(1, 8);
+                    if (classChoice == 5 || classChoice == 7)
+                    {
+                        classChoice = 0;
+                    }
+                } else if (intelligence > 8 && dexterity < 9 && constitution > 8)    // No Halfling
+                {
+                    classChoice = rnd.Next(1, 7);
+                } else if (intelligence > 8 && dexterity > 8 && constitution > 8)    // Allows all classes
+                {
+                    classChoice = rnd.Next(1, 8);
                 }
+
+                switch (classChoice)
+                {
+                    case 1:
+                        result = "Cleric";
+                        break;
+                    case 2:
+                        result = "Fighter";
+                        break;
+                    case 3:
+                        result = "Magic User";
+                        break;
+                    case 4:
+                        result = "Thief";
+                        break;
+                    case 5:
+                        result = "Elf";
+                        break;
+                    case 6:
+                        result = "Dwarf";
+                        break;
+                    case 7:
+                        result = "Halfling";
+                        break;
+                    default:
+                        result = "Fighter";
+                        break;
+                }
+
+                return result;
+            }
+
+            int GetHP(string job, int stat)
+            {
+                // This function calculates the starting hit points of the character based on
+                //  the class' hit die modified by constitution
+
+                int result;
+
+                if (job == "Cleric" || job == "Elf" || job == "Halfling") {
+                    result = RollDice(1, 6) + stat;
+                } else if(job == "Fighter" || job == "Dwarf") {
+                    result = RollDice(1, 8) + stat;
+                } else {
+                    result = RollDice(1, 4) + stat;
+                }
+
+                if (result <= 0) {      // Prevents negative starting HP
+                    result = 1;
+                }
+
+                return result;
             }
         }
 
@@ -116,7 +248,35 @@ namespace Dnd_Character_Generator
             return result;
         }
 
+        static void ReadCsv(string filePath)
+        {
+            StreamReader readFile = new StreamReader(filePath);
+            string line;
+            string[] row;
+            readFile.ReadLine();
+            while ((line = readFile.ReadLine()) != null)
+            {
+                row = line.Split(',');
+                string Class = row[0];
+                int level = Convert.ToInt32(row[1]);
+                int death = Convert.ToInt32(row[2]);
+                int wands = Convert.ToInt32(row[3]);
+                int paralysis = Convert.ToInt32(row[4]);
+                int breath = Convert.ToInt32(row[5]);
+                int spells = Convert.ToInt32(row[6]);
+                int thaco = Convert.ToInt32(row[7]);
+                int xp = Convert.ToInt32(row[8]);
 
+                Console.WriteLine(death);
+                Console.WriteLine(wands);
+                Console.WriteLine(paralysis);
+                Console.WriteLine(breath);
+                Console.WriteLine(spells);
+                Console.WriteLine(thaco);
+                Console.WriteLine(xp);
+            }
+
+        }
     }
 
 }
